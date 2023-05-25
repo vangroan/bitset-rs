3234cb31b15f887e962a1d98e383f471a725c30c
Willem Victor <wimpievictor@gmail.com>
2023-05-25 23:08:25 +0200

    Implemented multiple useful functions

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,9 @@
 #[cfg(test)]
 mod tests;
 
-use num_traits::PrimInt;
+use std::fmt::Debug;
+
+use num_traits::{PrimInt, ToPrimitive};
 
 pub struct Bitset<const N: usize, T = usize> {
     data: [T; N],
@@ -13,29 +15,25 @@ pub struct Bitset<const N: usize, T = usize> {
 
 impl<const N: usize, T> Bitset<N, T>
 where
-    T: PrimInt + Default,
+    T: PrimInt,
 {
     const BITS: usize = std::mem::size_of::<T>() * u8::BITS as usize;
 
     #[inline]
-    pub fn new() -> Self {
-        assert!(Self::BITS.is_power_of_two());
-        Self {
-            data: [Default::default(); N],
-        }
+    fn index_offset(&self, pos: usize) -> (usize, usize) {
+        let index = pos / Self::BITS;
+        let bit_offset = pos & (Self::BITS - 1);
+
+        (index, bit_offset)
     }
 
-    pub fn len(&self) -> usize {
-        self.data.len()
-    }
-
-    pub fn is_empty(&self) -> bool {
-        self.data.is_empty()
+    /// Set all bits to false.
+    pub fn reset(&mut self) {
+        self.data.fill(T::zero())
     }
 
     pub fn set(&mut self, pos: usize, value: bool) {
-        let index = pos / Self::BITS;
-        let bit_offset = pos & (Self::BITS - 1);
+        let (index, bit_offset) = self.index_offset(pos);
 
         let element = self.data[index];
 
@@ -46,13 +44,84 @@ where
         }
     }
 
-    pub fn clear(&mut self) {
-        self.data.fill(T::default())
+    pub fn len(&self) -> usize {
+        self.data.len()
+    }
+
+    /// Returns `true` if the bitset length is 0.
+    pub fn is_empty(&self) -> bool {
+        self.data.is_empty()
+    }
+
+    /// Number of bits in the set.
+    pub fn bit_len(&self) -> usize {
+        self.data.len() * Self::BITS
+    }
+
+    /// Iterator over the bits, returned as `1` or `0`.
+    pub fn bits(&self) -> BitIter<'_, N, T> {
+        BitIter {
+            bitset: self,
+            position: 0,
+        }
+    }
+
+    /// Sets all `true` bits to `false`, and all `false` bits to `true`.
+    pub fn flip(&mut self) {
+        for element in &mut self.data {
+            *element = element.not();
+        }
+    }
+
+    /// Returns `true` if any bits are set to true.
+    pub fn any(&self) -> bool {
+        self.data.iter().any(|el| el.gt(&T::zero()))
+    }
+
+    /// Returns `true` if all bits are set to true.
+    pub fn all(&self) -> bool {
+        self.data.iter().all(|el| el.eq(&T::max_value()))
+    }
+
+    /// Returns `true` if no bits are set to true.
+    pub fn none(&self) -> bool {
+        self.data.iter().all(|el| el.eq(&T::zero()))
     }
 
     /// Retrieve the underlying array storage.
     pub fn as_array(&self) -> &[T; N] {
         &self.data
+    }
+}
+
+impl<const N: usize, T> Bitset<N, T>
+where
+    T: PrimInt + Default,
+{
+    #[inline]
+    pub fn new() -> Self {
+        assert!(Self::BITS.is_power_of_two());
+        Self {
+            data: [Default::default(); N],
+        }
+    }
+}
+
+impl<const N: usize, T> Bitset<N, T>
+where
+    T: PrimInt + ToPrimitive,
+{
+    #[inline]
+    pub fn get_bit(&self, pos: usize) -> u8 {
+        let (index, bit_offset) = self.index_offset(pos);
+
+        let element = self.data[index];
+
+        element
+            .rotate_right(bit_offset as u32)
+            .bitand(T::one())
+            .to_u8()
+            .unwrap_or_default()
     }
 }
 
@@ -62,5 +131,37 @@ where
 {
     fn default() -> Self {
         Self::new()
+    }
+}
+
+impl<const N: usize, T> Debug for Bitset<N, T>
+where
+    T: PrimInt + Debug + 'static,
+{
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_list().entries(self.bits()).finish()
+    }
+}
+
+/// Iterator producing the bits as either a `0` or `1`.
+pub struct BitIter<'a, const N: usize, T: 'static> {
+    bitset: &'a Bitset<N, T>,
+    position: usize,
+}
+
+impl<'a, const N: usize, T> Iterator for BitIter<'a, N, T>
+where
+    T: PrimInt + 'static,
+{
+    type Item = u8;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.position < self.bitset.bit_len() {
+            let bit = self.bitset.get_bit(self.position);
+            self.position += 1;
+            Some(bit)
+        } else {
+            None
+        }
     }
 }
--- src/tests.rs
+++ src/tests.rs
@@ -21,9 +21,78 @@ fn test_set() {
     bitset.set(0, false);
     assert_eq!(bitset.as_array(), &[2, 1]);
 
-    bitset.clear();
+    bitset.reset();
     assert_eq!(bitset.as_array(), &[0, 0]);
 
     bitset.set(33, true);
     assert_eq!(bitset.as_array(), &[0, 2]);
+}
+
+#[test]
+fn test_any() {
+    let mut bitset = Bitset::<2, u32>::new();
+
+    assert!(!bitset.any());
+    bitset.set(0, true);
+    assert!(bitset.any());
+}
+
+#[test]
+fn test_all() {
+    let mut bitset = Bitset::<2, u32>::new();
+
+    assert!(!bitset.all());
+    bitset.set(0, true);
+    assert!(!bitset.all());
+
+    for i in 0..64 {
+        bitset.set(i, true);
+    }
+    assert!(bitset.all());
+}
+
+#[test]
+fn test_none() {
+    let mut bitset = Bitset::<2, u32>::new();
+
+    assert!(bitset.none());
+    bitset.set(0, true);
+    assert!(!bitset.none());
+}
+
+#[test]
+fn test_flip() {
+    let mut bitset = Bitset::<2, u32>::new();
+
+    bitset.set(0, true);
+    assert_eq!(bitset.as_array(), &[1, 0]);
+
+    bitset.flip();
+    assert_eq!(bitset.as_array(), &[0xFFFFFFFE, 0xFFFFFFFF]);
+}
+
+#[test]
+fn test_bit_iter() {
+    let mut bitset = Bitset::<2, u32>::new();
+
+    bitset.set(0, true);
+    bitset.set(7, true);
+    bitset.set(11, true);
+    bitset.set(33, true);
+    bitset.set(42, true);
+
+    println!("{bitset:?}");
+
+    let mut count = 0;
+
+    for (i, bit) in bitset.bits().enumerate() {
+        match i {
+            0 | 7 | 11 | 33 | 42 => assert_eq!(bit, 1, "index: {i}; bit: {bit}"),
+            _ => assert_eq!(bit, 0, "index: {i}; bit: {bit}"),
+        }
+
+        count += 1;
+    }
+
+    assert_eq!(count, 64);
 }
